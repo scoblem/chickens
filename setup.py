799f3c8f4bf97792bf71b10634695307600ae80c
Mark Scoble <scoble.mr@gmail.com>
2015-10-26 20:16:05 +0800

    chicken

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = '__chicken__',
+  packages = ['__chicken__'],
+  version = '1.0',
+  description = 'CHICKEN',
+  author = 'Mark Scoble',
+  author_email = 'scoble.mr@gmail.com',
+  url = 'https://github.com/Necrolisk/chicken',
+  download_url = 'https://github.com/Necrolisk/chicken/tarball/1.0',
+  keywords = ['chicken', 'CHICKEN']
+  classifiers = [],
+)
