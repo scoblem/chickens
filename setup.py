22195cfe5297148415f79a7ad09551d2be56a1f5
Mark Scoble <scoble.mr@gmail.com>
2015-10-26 21:52:47 +0800

    chickens

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
-from setuptools import setup, find_packages
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.md').read()
+
 setup(
-  name = 'chickens',
-  packages = ['chickens'],
-  version = '1.7',
-  description = 'chickens',
-  author = 'Mark Scoble',
-  author_email = 'scoble.mr@gmail.com',
-  url = 'https://github.com/Necrolisk/chickens',
-  download_url = 'https://github.com/Necrolisk/chickens/tarball/1.7',
-  keywords = ['chicken', 'CHICKEN', 'CHICKENS', 'chickens'],
-  classifiers = [],
+    name='chickens',
+    version='1.7',
+    description='chickens',
+    long_description=readme,
+    author='Mark Scoble',
+    author_email='scoble.mr@gmail.com',
+    url='https://github.com/Necrolisk/chickens',
+    packages=['chickens'],
+    package_dir={'chickens': 'chickens'},
+    include_package_data=True,
+    install_requires=[
+        'egg'
+    ],
+    zip_safe=False,
+    keywords=['chicken', 'CHICKEN', 'CHICKENS', 'chickens'],
+    classifiers=[],
 )
